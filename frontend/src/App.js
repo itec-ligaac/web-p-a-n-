5a7f2803150b4c8b5a7b079d6ba16c19908d762e
Petrified0110 <[petra.horvath99@gmail.com]>
2021-03-27 21:30:25 +0200

    Set up an ugly autocomplete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,22 @@ import './App.css'
 import SearchBar from './components/SearchBar'
 import LocationContainer from './components/LocationContainer'
 import { useState } from 'react'
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 function App() {
   const [searchLocation, setSearchLocation] = useState("");
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyAn3Z4M460U1yWWNulaTOs09Aj6atUIXmk",
+    libraries: ["places"]
+  });
+  if(!isLoaded) return <p>Loading...</p>;
+  console.log(window);
   return (
-    <div className="App">
-      <Header />
-      <SearchBar searchLocation={searchLocation} setSearchLocation={setSearchLocation} />
-      <LocationContainer setSearchLocation={setSearchLocation} />
-    </div>
+        <div className="App">
+          <Header />
+          <SearchBar searchLocation={searchLocation} setSearchLocation={setSearchLocation} />
+          <LocationContainer setSearchLocation={setSearchLocation} />
+        </div>
   );
 }
 
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete'
 
 function formatDate(date) {
     var d = new Date(date),
@@ -12,14 +13,64 @@ function formatDate(date) {
         day = '0' + day;
 
     return [year, month, day].join('-');
-}
+}    
 
-const SearchBar = ({ searchLocation, setSearchLocation }) => {
+const SearchBar = () => {
     const [date, setDate] = useState(formatDate(Date.now()));
 
-    return (
+    const {
+        ready,
+        value,
+        suggestions: { status, data },
+        setValue, clearSuggestions
+      } = usePlacesAutocomplete({
+        requestOptions: { /* Define search scope here */ }
+      });
+  
+      const handleInput = e => {
+          // Update the keyword of the input element
+          setValue(e.target.value);
+        };
+  
+        const handleSelect = ({ description }) => () => {
+          // When user selects a place, we can replace the keyword without request data from API
+          // by setting the second parameter as "false"
+          setValue(description, false);
+      
+          // Get latitude and longitude via utility functions
+          getGeocode({ address: description })
+            .then(results => getLatLng(results[0]))
+            .then(({ lat, lng }) => {
+              console.log('📍 Coordinates: ', { lat, lng });
+            }).catch(error => {
+              console.log('😱 Error: ', error)
+            });
+        };
+  
+      const renderSuggestions = () =>
+      data.map(suggestion => {
+        const {
+          id,
+          structured_formatting: { main_text, secondary_text }
+        } = suggestion;
+  
+        return (
+          <li
+            key={id}
+            onClick={handleSelect(suggestion)}
+          >
+            <strong>{main_text}</strong> <small>{secondary_text}</small>
+          </li>
+        );
+      });
+
+      return (
         <div className="Search">
-            <input type="text" placeholder="Location" className="Search-Bar" value={searchLocation} onChange={(e) => setSearchLocation(e.target.value)} />
+            <div>
+                <input type="text" placeholder="Location" className="Search-Bar" value={value} 
+                onChange={handleInput} />
+                {<ul>{renderSuggestions()}</ul>}
+            </div>
             <select name="dangerousnes" className="Dropdown">
                 <option>Ascending</option>
                 <option>Descending</option>
