a79aebfa789a02d941b6e108b48dc0ef65106dc0
Vladi <dariuslogon12@gmail.com>
2021-03-27 13:35:01 +0200

    Layout branch

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <header class="App-header">
+            Covidtector
+        </header>
+    )
+}
+
+export default Header
